269872d0ff0d20dd96aa3f71c783876dce72f5f8
Sachin Ahuja <sachin.ahuja1992@gmail.com>
2017-04-13 14:18:27 -0400

    Algo to calculate scores and few changes in view.py

--- balanceofpower/views.py
+++ balanceofpower/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .forms import InfoForm
+import pandas as pd
 
 from django.contrib.staticfiles import finders
 # Create your views here.
@@ -17,6 +18,17 @@ def results(request):
         pass  # Write code for processing the form information. The form information should be passed to appropriate functions for processing and finally call results function for display
     #Write information for displaying of results. All the required data is in the form.
     return render(request, 'results.html')
+
+def input_output(selected_countries,selected_categories,weights):
+	economy=pd.read_csv("data/economy.csv")
+	society=pd.read_csv("data/society.csv")
+	geography=pd.read_csv("data/geography.csv")
+	demographics=pd.read_csv("data/demographics.csv")
+	infrastructure=pd.read_csv("data/infrastructure.csv")
+	dataframes=[economy,society,geography,demographics,infrastructure]
+	truth_table=pd.concat(dataframes,axis=1,join='outer')
+	
+
 
 
 #Only for static files testing. No use in balance of power application
