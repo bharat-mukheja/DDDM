2c1d3342e5f7e7592e662cbd473ef10c2a41739b
Bharat Mukheja <bkmukhej@ncsu.edu>
2017-04-18 19:56:38 -0400

    recreate

---
+++ balanceofpower/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BalanceofpowerConfig(AppConfig):
+    name = 'balanceofpower'
---
+++ balanceofpower/urls.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name = 'index'),
+    url(r'^formfill/$', views.formfill, name='formfill'),
+    url(r'^results/$', views.results, name='results'),
+    url(r'^test/$', views.test, name='test'),
+    url(r'^latest/$', views.latest, name='latest'),
+]
